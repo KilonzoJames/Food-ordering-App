5f5b61f6eef8b1adc644b6785d8f79d44c680356
KilonzoJames <jay.kilonzo@gmail.com>
2023-08-01 15:44:47 +0300

    Renamed class to className

--- src/components/Foodlist.js
+++ src/components/Foodlist.js
@@ -8,17 +8,17 @@ function Foodlist({foods,setFood}) {
 
     const allFoods=foods.map((food)=>
     (
-        <div class="ui card m-2 border border-primary">
-            <div class="image">
+        <div className="ui card m-2 border border-primary">
+            <div className="image">
                 <img src="https://tastesbetterfromscratch.com/wp-content/uploads/2023/04/Hershey-Chocolate-Cake-8-2-500x500.jpg" alt='Logo'/>
             </div>
-            <div class="content">
-                <div class="header">{food.food}</div>
-                <div class="meta"><span class="date">{food.price}</span></div>
-                <div class="description">{food.restaurant_id}</div>
+            <div className="content">
+                <div className="header">{food.food}</div>
+                <div className="meta"><span className="date">{food.price}</span></div>
+                <div className="description">{food.restaurant_id}</div>
             </div>
-            <button type="button" class="btn btn-primary">Add to Cart</button>
-            <button type="button" class="btn btn-warning">Checkout</button>
+            <button type="button" className="btn btn-primary">Add to Cart</button>
+            <button type="button" className="btn btn-warning">Checkout</button>
         </div>
       )
     )
