325f30c70aefcaaba0e677aa0c643b5365737ab8
KilonzoJames <jay.kilonzo@gmail.com>
2023-08-01 14:48:05 +0300

    Added link visibilty

--- src/components/Foodlist.js
+++ src/components/Foodlist.js
@@ -2,31 +2,29 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 function Foodlist({foods,setFood}) {
-console.log(foods)
     const allFoods=foods.map((food)=>
     (
-        <div class="ui card m-2">
+        <div class="ui card m-2 border border-primary">
             <div class="image">
                 <img src="https://tastesbetterfromscratch.com/wp-content/uploads/2023/04/Hershey-Chocolate-Cake-8-2-500x500.jpg" alt='Logo'/>
-                </div>
-                <div class="content">
-                    <div class="header">{food.food}</div>
-                    <div class="meta"><span class="date">{food.price}</span></div>
-                    <div class="description">{food.restaurant_id}</div></div>
-                    <button type="button" class="btn btn-primary">Add to Cart</button>
-                    <button type="button" class="btn btn-info">Primary</button>
-                    <div class="extra content">
-                    {/* <a><i aria-hidden="true" class="user icon"></i>22 Friends</a> */}
-                    </div></div>
+            </div>
+            <div class="content">
+                <div class="header">{food.food}</div>
+                <div class="meta"><span class="date">{food.price}</span></div>
+                <div class="description">{food.restaurant_id}</div>
+            </div>
+            <button type="button" class="btn btn-primary">Add to Cart</button>
+            <button type="button" class="btn btn-warning">Checkout</button>
+        </div>
       )
     )
 
   return  (
-    <div className='col-12 border border-primary bg-success'>
+    <div className='col-12 bg-light'>
       <div className='row'>
         <nav className='foodchoices'>
-            <NavLink to="/">Login Page</NavLink> 
-            <NavLink to="/foodchoice">Food choices</NavLink> 
+            <NavLink to="/" className="nav-link text-primary">Login Page</NavLink> 
+            <NavLink to="/foodchoice" className="nav-link text-primary">Food choices</NavLink> 
         </nav>
         {allFoods}
       </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -11,25 +11,46 @@ function Login() {
       <div className="row justify-content-center">
         <div className="col-md-6">
           <div className="d-flex align-items-center justify-content-center" style={{ height: '100vh' }}>
-            <form style={{ minWidth: '300px', maxWidth: '800px' }}>
+            <form className='borderstyling border border-primary' style={{ minWidth: '500px', maxWidth: '800px', }}>
               <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                <label 
+                htmlFor="exampleInputEmail1" 
+                className="form-label">
+                Email address
+                </label>
+                <input 
+                type="email" 
+                className="form-control"
+                />
               </div>
+               
               <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" />
+                <label 
+                htmlFor="exampleInputPassword1" 
+                className="form-label">
+                Password
+                </label>
+                <input 
+                type="password" 
+                className="form-control"
+                />
               </div>
+
               <div className="mb-3 form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                <input 
+                type="checkbox" 
+                className="form-check-input" />
+                <label 
+                className="form-check-label" 
+                htmlFor="exampleCheck1">
+                Check me out
+                </label>
               </div>
+
               <button
                 type="submit"
                 className="btn btn-primary"
-                onClick={handleSubmit}
-              >
+                onClick={handleSubmit}>
                 <NavLink to="/foodlist" className="nav-link text-white">Submit</NavLink>
               </button>
             </form>
