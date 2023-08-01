b0bb99169160e96ac3ec559f60e4e4ececdc310b
Addonay <addonay.osoro@student.moringaschool.com>
2023-08-01 12:58:40 +0300

    centering the form

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,30 +7,33 @@ function Login() {
   }
 
   return (
-    <div className="container">
+    <div className="container mt-5">
       <div className="row justify-content-center">
         <div className="col-md-6">
-          <form>
-            <div className="mb-3">
-              <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-              <input type="email" className="form-control" required />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-              <input type="password" className="form-control" required />
-            </div>
-            <div className="mb-3 form-check">
-              <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-              <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-            </div>
-            <button
-              type="submit"
-              className="btn btn-primary"
-              onClick={handleSubmit}
-            >
-              <NavLink to="/foodlist" className="nav-link text-white">Submit</NavLink>
-            </button>
-          </form>
+          <div className="d-flex align-items-center justify-content-center" style={{ height: '100vh' }}>
+            <form style={{ minWidth: '300px', maxWidth: '800px' }}>
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" />
+              </div>
+              <div className="mb-3 form-check">
+                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary"
+                onClick={handleSubmit}
+              >
+                <NavLink to="/foodlist" className="nav-link text-white">Submit</NavLink>
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
