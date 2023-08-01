06187ff4909a95d5e2d7ca3b4f5d9d1ff99fdfa3
Addonay <addonay.osoro@student.moringaschool.com>
2023-08-01 12:10:43 +0300

    done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,37 +1,41 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Login() {
-    function handleSubmit(event){
-console.log("object")    }
-
+  function handleSubmit(event) {
+    console.log("object");
+  }
 
   return (
-    <div>
-        <form>
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <form>
             <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+              <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+              <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1"/>
+              <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+              <input type="password" className="form-control" id="exampleInputPassword1" />
             </div>
             <div className="mb-3 form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+              <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+              <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
             </div>
-            <button 
-            type="submit" 
-            className="btn btn-primary"
-            onSubmit={handleSubmit}>
-            <NavLink to="/foodlist">Submit</NavLink> 
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={handleSubmit}
+            >
+              <NavLink to="/foodlist" className="nav-link text-white">Submit</NavLink>
             </button>
-        </form>
-      
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
