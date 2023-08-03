cbb788539ed18a9a89def758eb685700c221de0e
Addonay <addonay.osoro@student.moringaschool.com>
2023-08-03 13:53:36 +0300

    modified the homepage and the foodlist

--- src/components/Foodlist.js
+++ src/components/Foodlist.js
@@ -1,88 +1,99 @@
-import React, { useState } from 'react';
-import { NavLink, useParams } from 'react-router-dom';
-import Search from './Search';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart, faPlus, faMinus, faCheck } from "@fortawesome/free-solid-svg-icons";
 
-function Foodlist({ foods, choices, restaurants ,setChoices, quantities, setQuantities }) {
-  const [hint, setHint] = useState('');
+function Foodlist({ foods, choices, setChoices, quantities, setQuantities }) {
+  const [hint, setHint] = React.useState("");
+  const [addedToCart, setAddedToCart] = React.useState(false); // Track whether the button is clicked or not
   const { id } = useParams();
-  const food = foods.find((food) => food.id === parseInt(id));
-
-  function changeQuantity(foodId, change) {
-    setQuantities((prevQuantities) => {
-      const newQuantity = (prevQuantities[foodId] || 0) + change;
-      return {
-        ...prevQuantities,
-        [foodId]: newQuantity < 0 ? 0 : newQuantity,
-      };
-    });
-  }
-
-  // Filter the foods based on the selected restaurant ID
   const filteredFoods = foods.filter((food) => food.restaurant_id === parseInt(id));
+  const filteredFoodsWithSearch = filteredFoods.filter((food) =>
+    food.food.toLowerCase().includes(hint.toLowerCase())
+  );
 
-  function addToCart(event) {
-    const foodId = parseInt(event.target.id);
-    const selectedFood = foods.find((food) => food.id === foodId);
-    const isFoodAlreadyAdded = choices.some((food) => food.id === foodId);
-    !isFoodAlreadyAdded
-      ? setChoices([...choices, selectedFood])
-      : console.log('Already in favourites');
-    event.target.classList.add('clicked');
-  }
-  const filteredFoodsByHint = filteredFoods.filter((food) => {
-    if (food.food) {
-      const lowerCaseValue = hint.toLowerCase();
-      return food.food.toLowerCase().includes(lowerCaseValue);
-    }
-    return false;
-  });
-  
-  const allFoods = filteredFoodsByHint.map((food) => (
-    <div className="card col-2 border border-primary" key={food.id}>
-      <div className="image">
+  const allFoods = filteredFoodsWithSearch.map((food) => (
+    <div className="col-12 col-sm-6 col-md-4 col-lg-3" key={food.id}>
+      <div className="card border-primary mb-4">
         <img
+          className="card-img-top"
           src={food.image}
           alt={food.food}
-          style={{ width: '100%', height: '250px', objectFit: 'cover' }}
+          style={{ height: "200px", objectFit: "cover" }}
         />
-      </div>
-      <div className="content">
-        <div className="header">Food: {food.food}</div>
-        <div className="meta">
-          <span className="date">Price: {food.price}</span>
+        <div className="card-body d-flex flex-column align-items-center"> {/* Added flexbox class */}
+          <h5 className="card-title">{food.food}</h5>
+          <p className="card-text">Price: {food.price}</p>
+          <p className="card-text">Restaurant Name: {food.restaurant_id}</p>
+          <div className="quantity d-flex align-items-center"> {/* Added flexbox class */}
+            <button onClick={() => changeQuantity(food.id, -1)}>
+              <FontAwesomeIcon icon={faMinus} />
+            </button>
+            <span>{quantities[food.id] || 1}</span>
+            <button onClick={() => changeQuantity(food.id, 1)}>
+              <FontAwesomeIcon icon={faPlus} />
+            </button>
+          </div>
+          <button
+            onClick={() => addToCart(food)}
+            type="button"
+            className={`btn btn-primary ${addedToCart && isFoodAddedToCart(food) ? "added" : ""}`}
+          >
+            {addedToCart && isFoodAddedToCart(food) ? <FontAwesomeIcon icon={faCheck} /> : "Add to Cart"}
+          </button>
         </div>
-        <div className="description">Restaurant_name: {restaurants.name}</div>
       </div>
-      <div className="quantity">
-      <button onClick={() => changeQuantity(food.id, -1)}>-</button>
-      <span>{quantities[food.id] || 0}</span>
-      <button onClick={() => changeQuantity(food.id, 1)}>+</button>
-      </div>
-      <button onClick={addToCart} id={food.id} type="button" className="btn btn-primary">
-        Add to Cart
-      </button>
-      <button type="button" className="btn btn-info">
-        <NavLink to="/foodchoice" className="nav-link text-light">
-          View Cart
-        </NavLink>
-      </button>
     </div>
   ));
 
+  function addToCart(selectedFood) {
+    const isFoodAlreadyAdded = choices.some((food) => food.id === selectedFood.id);
+  
+    if (!isFoodAlreadyAdded) {
+      setChoices([...choices, selectedFood]);
+      setQuantities({ ...quantities, [selectedFood.id]: 1 }); // Set default quantity to 1 for new items
+    }
+  
+    setAddedToCart(true); // Set the addedToCart state to true when the button is clicked
+  
+    // Reset the "Add to Cart" button back to its original state after 2 seconds
+    setTimeout(() => {
+      setAddedToCart(false);
+    }, 2000);
+  }
+  
+
+  function isFoodAddedToCart(selectedFood) {
+    return choices.some((food) => food.id === selectedFood.id);
+  }
+
+  function changeQuantity(foodId, value) {
+    setQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [foodId]: Math.max((prevQuantities[foodId] || 0) + value, 1), // Ensure quantity is at least 1
+    }));
+  }
+
   return (
-    <div className="col-12 bg-light">
-      <div className="row">
-        <nav className="align-items-center justifd-flex y-content-center">
-          <NavLink to="/homepage" className="nav-link text-primary">
-            Home Page
-          </NavLink>
-        </nav>
-        <Search hint={hint} setHint={setHint} />
-        <div className="food-list">{allFoods}</div>
+    <div className="container">
+      <h1>Food List</h1>
+      <div className="mb-3 d-flex align-items-center">
+        <input
+          type="text"
+          value={hint}
+          onChange={(e) => setHint(e.target.value)}
+          className="form-control form-control-sm me-5"
+          placeholder="Search for food..."
+          aria-label="Search for food"
+        />
+        <NavLink to="/foodchoice" className="cart-icon">
+          <FontAwesomeIcon icon={faShoppingCart} beat size="2x" />
+        </NavLink>
       </div>
+      <div className="row">{allFoods}</div>
     </div>
   );
 }
 
 export default Foodlist;
-
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart, faBookOpen } from "@fortawesome/free-solid-svg-icons";
 
 const HomePage = ({ username, restaurants, setRestaurants }) => {
   const [searchTerm, setSearchTerm] = React.useState("");
@@ -11,17 +13,20 @@ const HomePage = ({ username, restaurants, setRestaurants }) => {
   return (
     <div className="container">
       <h1>{username!=="" ? `${username}, Welcome to the Food Court!` : "Welcome to the Food Court!"}</h1>
-      <div className="mb-3">
+      <div className="mb-3 d-flex align-items-center">
         <input
           type="text"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          className="form-control"
+          className="form-control form-control-sm me-5"
           placeholder="Search for restaurants..."
           aria-label="Search for restaurants"
         />
+        <NavLink to="/foodchoice" className="cart-icon">
+          <FontAwesomeIcon icon={faShoppingCart} beat size="2x" />
+        </NavLink>
       </div>
-      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 gy-4">
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 gy-4">
         {filteredRestaurants.map((restaurant) => (
           <div className="col" key={restaurant.id}>
             <div className="card border-primary h-100">
@@ -34,9 +39,12 @@ const HomePage = ({ username, restaurants, setRestaurants }) => {
               <div className="card-body">
                 <h2 className="card-title">{restaurant.name}</h2>
                 <p className="card-text">{restaurant.description}</p>
-                <NavLink to={`/foodlist/${restaurant.id}`} className="btn btn-primary">
-                  View Menu
-                </NavLink>
+                <div className="d-grid">
+                  <NavLink to={`/foodlist/${restaurant.id}`} className="btn btn-primary">
+                    <FontAwesomeIcon icon={faBookOpen} className="me-2" />
+                    View Menu
+                  </NavLink>
+                </div>
               </div>
             </div>
           </div>
